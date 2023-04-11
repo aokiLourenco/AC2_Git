9e69d9b1f65f8d683ff6f1d1b9a987e7f940cab3
Anderson Aoki <aaokilourenco@ua.pt>
2023-04-11 18:30:18 +0100

    :100:

--- ex_add/teste_2022/f11.c
+++ ex_add/teste_2022/f11.c
@@ -8,24 +8,26 @@ void delay(unsigned int ms)
 
 int main(void)
 {
-    int counter = 1;
+    int counter = 0b00000001;
     TRISBbits.TRISB2 = 1;  
     TRISE = TRISE & 0xFFC0;          // 1111 1111 1100 0000
 
     while(1)
     {
+        counter = counter << 1;
+        if (counter > 0b10000000){
+            counter = 0b00000001;
+        }
+
         if (PORTBbits.RB2 == 0)
         {
+            LATE = (LATE & 0xFFC0) | counter;
             delay(333);
         } else
         {
+            LATE = (LATE & 0xFFC0) | counter;
             delay(143);
         }
-
-        LATE = (LATE & 0xFFC0) | counter;
-        counter = counter << 1;
-        if (counter > 8);
-            counter = 1;
     }
 
     return 0;
